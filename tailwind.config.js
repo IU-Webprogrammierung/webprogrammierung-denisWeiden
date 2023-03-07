3cd50fbcaa53e165e086c7bf084a73dc36accff3
Denis Weiden <denis.weiden@iubh-fernstudium.de>
2023-03-07 17:40:56 +0100

    added color to tailwind.config + NAV in index.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	theme: {
+		extends: {
+			color: {
+				rainmaker: {
+					100: "#EDEEF0",
+					200: "#DADDE1",
+					300: "#B5BBC2",
+					400: "#9198A4",
+					500: "#6C7685",
+					600: "#475467",
+					700: "#394352",
+					800: "#2B323E",
+					900: "#1C2229",
+					1000: "#0E1115"
+				}
+			}
+		}
+	}
 }
