bd47f5f708886f121dc4cde1e9614f8bffd834f9
Denis Weiden <denis.weiden@iubh-fernstudium.de>
2023-03-30 19:00:15 +0200

    added tailwindcss framework

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '760px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      colors: {
+        primaryColor: '#475467',
+        customBlack: '#101828'
+      }
+    },
+  },
+  plugins: [],
+}
+
