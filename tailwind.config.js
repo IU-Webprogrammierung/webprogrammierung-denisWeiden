a203dec35cce49422643f3b38d0585d4b7f3c93d
Denis Weiden <denis.weiden@iubh-fernstudium.de>
2023-04-14 21:04:58 +0200

    added breakpoint

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,28 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
-  content: ['./*.html'],
+  content: ["./*.html"],
   theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+    },
     screens: {
-      sm: '480px',
-      md: '760px',
-      lg: '976px',
-      xl: '1440px'
+      xs: "400px",
+      sm: "480px",
+      md: "760px",
+      lg: "976px",
+      xl: "1440px",
     },
     extend: {
       colors: {
-        primaryColor: '#475467',
-        customBlack: '#101828',
-        label: '#344054',
-        resume: '#F9FAFB'
-      }
+        primaryColor: "#475467",
+        customBlack: "#101828",
+        label: "#344054",
+        resume: "#F9FAFB",
+      },
     },
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-  ],
-}
-
+  plugins: [require("@tailwindcss/forms")],
+};
